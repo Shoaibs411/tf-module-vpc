77bdd36e970d4fb1eb4e0c53ebace73c277a3414
Shoaib <shoaibs411@gmail.com>
2024-01-04 00:43:55 +0530

    Provisioning VPC - updated Subnet details

--- subnets.tf
+++ subnets.tf
@@ -1,17 +1,23 @@
 resource "aws_subnet" "public_subnet" {
+  
+  count      = length(var.PUBLIC_SUBNET_CIDR)
+  
   vpc_id     = aws_vpc.main.id
-  cidr_block = 
+  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
 
   tags = {
-    Name = "Main"
+    Name = "roboshop-public-subnet"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
+  
+  count      = length(var.PRIVATE_SUBNET_CIDR)
+
   vpc_id     = aws_vpc.main.id
-  cidr_block = 
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
 
   tags = {
-    Name = "Main"
+    Name = "roboshop-private-subnet"
   }
 }
