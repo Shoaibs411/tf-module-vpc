b660e0c40bab2134577ed0a4cb4834ba7d041d19
Shoaib <shoaibs411@gmail.com>
2024-02-01 02:13:34 +0530

    TF Infra AutoPush - Minor updates for visibility

--- route-table.tf
+++ route-table.tf
@@ -51,7 +51,7 @@ resource "aws_route_table_association" "private_subnet_rt_association" {
   count                         = length(aws_subnet.private_subnet.*.id)
 
   subnet_id                     = element(aws_subnet.private_subnet.*.id, count.index)
-  route_table_id                = aws_route_table.private_subnet.id
+  route_table_id                = aws_route_table.private_rt.id
 }
 
 # Adding a Route to the default VPC route table
